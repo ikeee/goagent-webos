697dd09fde21776d4cb82842a3f96aec4a9b8ca0
hewigovens <hewigovens@gmail.com>
2011-11-20 17:44:33 +0800

    package script

--- applications/com.goagent.local/source/ComGoagentApp.js
+++ applications/com.goagent.local/source/ComGoagentApp.js
@@ -1,18 +1,24 @@
-enyo.kind(
-{
-	name: "ComGoagentApp",
-	kind: enyo.VFlexBox,
-	components:
-	[
-		{
-			kind: "ToggleButton",
-			onLabel:"On",
-			offLabel:"Off",
-			onChange:"buttonToggle",
-		
-			buttonToggle : function(inSender,inState){
-				this.log("Toggled to state" + inState);
-			}
-		}
-	]
-});
+enyo.kind
+(
+    {
+    	name: "ComGoagentApp",kind: enyo.VFlexBox,components: [
+
+		    {kind: "PageHeader", components: [
+					{kind:"Image", src:"settings32.png"},        			
+        			{content: "Settings"}
+			 ]},
+			 
+		    {kind: "Pane",components: [   
+				     {kind: "InputBox", components:[
+				     		{kind: "LabeledContainer", label:"Enable",width:"220px"},
+							{kind: "ToggleButton",onLabel:"On",offLabel:"Off",onChange:"buttonToggle"}
+				    ]}
+		        ]}    
+	    ],
+	    
+        buttonToggle:function(inSender,inState)
+        {
+            this.log("current state is "+inState);
+        }
+    }
+);
---
+++ gen_package.sh
@@ -0,0 +1,2 @@
+palm-package packages/com.goagent.local/ applications/com.goagent.local/ services/com.goagent.local/
+palm-install com.goagent.local_1.0.0_all.ipk
